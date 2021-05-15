c3f7aaa8fb61d737281ab81cb0821855095f98a9
Adrian0910 <uchihasantander@gmail.com>
2021-05-14 22:00:32 -0500

    Primer ejercicio Angular

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,15 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+let nombre:string = 'Strider';
+let hp: number | string =95; /* Con la condicionante admite ambos valores */
+let estaVivo: boolean = true;
+
+hp = 'FULL'; /* aún asi no se puede asiganar a otras cosas */
+
+
+console.log(nombre, hp);
+
+
+
